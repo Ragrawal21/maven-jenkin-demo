02742adda81493a9bc6d3046dd0b04e8884064a4
Ragrawal21 <rahul_agrawal8@persistent.com>
2021-06-29 12:26:57 +0530

    Adding maven project

---
+++ src/main/java/com/ps1/App.java
@@ -0,0 +1,13 @@
+package com.ps1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
